091bba5004d43a74056ab3123bdb8e9569dc2121
Benny Tsai <benny.tsai@gmail.com>
2011-03-16 00:16:16 -0600

    separate out onEnter function

--- src/kawa/android/battle.scm
+++ src/kawa/android/battle.scm
@@ -2,6 +2,7 @@
 
 (define *display* ::android.widget.TextView #!null)
 (define *scroller* ::android.widget.ScrollView #!null)
+(define *input* ::android.widget.EditText #!null)
 
 (activity
  battle
@@ -10,16 +11,19 @@
   (set! *display* (as <android.widget.TextView>
 		      ((this):findViewById kawa.android.R$id:display)))
   (set! *scroller* (as <android.widget.ScrollView>
-		       ((this):findViewById kawa.android.R$id:scroller))))
- ((onEnter (v ::android.view.View)) ::void
-  (let ((input (as <android.widget.EditText>
-		   ((this):findViewById kawa.android.R$id:input))))
-  (process input:text))))
+		       ((this):findViewById kawa.android.R$id:scroller)))
+  (set! *input* (as <android.widget.EditText>
+		    ((this):findViewById kawa.android.R$id:input)))
+  (new-game)))
+
+(define (onEnter (v ::android.view.View)) ::void
+  (show (string-append *input*:text "\n")))
 
 (define (show text ::string) ::void
   (*display*:append text)
   (*scroller*:post
-   (lambda () (*scroller*:fullScroll android.widget.ScrollView:FOCUS_DOWN))))
+   (lambda ()
+     (*scroller*:fullScroll android.widget.ScrollView:FOCUS_DOWN))))
 
-(define (process text ::string) ::void
-  (show (string-append text "\n")))
+(define (new-game)
+  (show "You are a mystic monk.\n"))
