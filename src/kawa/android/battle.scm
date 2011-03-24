2bf5919300b0df7e17697db158bb5f7aa439295b
Benny Tsai <benny.tsai@gmail.com>
2011-03-24 01:27:52 -0600

    implement foe generation and display logic

--- src/kawa/android/battle.scm
+++ src/kawa/android/battle.scm
@@ -15,13 +15,15 @@
   (new-game)))
 
 (define (onEnter v ::android.view.View)
-  (show (string-append *input*:text "\n")))
+  (output (string-append *input*:text "\n")))
 
-(define (show text)
-  (*display*:append text)
-  (*scroller*:post
-   (lambda ()
-     (*scroller*:fullScroll android.widget.ScrollView:FOCUS_DOWN))))
+(define (output x)
+  (let ((o (open-output-string)))
+    (display x o)
+    (*display*:append (get-output-string o))
+    (*scroller*:post
+     (lambda ()
+       (*scroller*:fullScroll android.widget.ScrollView:FOCUS_DOWN)))))
 
 ;; Global variables
 (define *health* #f)
@@ -35,10 +37,14 @@
 (define *attacks-left* #f)
 (define *attack-strength* #f)
 
+;; Utility functions
+(define (random n)
+  (*:nextInt (java.util.Random) (as int n)))
+
 ;; Main game functions
 (define (new-game)
   (init-player)
-  ;;  (init-foes)
+  (init-foes)
   (new-turn))
 
 (define (new-turn)
@@ -53,17 +59,83 @@
   (set! *strength* 30))
 
 (define (show-player)
-  (show "\nYou are a mystic monk with ")
-  (show (number->string *health*))
-  (show " health, ")
-  (show (number->string *agility*))
-  (show " agility, and ")
-  (show (number->string *strength*))
-  (show " strength.\n"))
+  (output "\nYou are a mystic monk with ")
+  (output *health*)
+  (output " health, ")
+  (output *agility*)
+  (output " agility, and ")
+  (output *strength*)
+  (output " strength.\n"))
 
 (define (new-attack)
-  ;;  (show-foes)
+  (show-foes)
   (show-attacks))
 
 (define (show-attacks)
-  (show "Attack style: [k]i strike [d]ual strike [f]lurry of blows\n"))
+  (output "Attack style: [k]i strike [d]ual strike [f]lurry of blows\n"))
+
+(define (randval n)
+  (+ 1 (random (max 1 n))))
+
+;; Foe management functions
+(define (init-foes)
+  (set! *foe-builders* (list orc hydra slime brigand))
+  (let ((build-foe (lambda (x)
+		     ((list-ref
+		       *foe-builders*
+		       (random (length *foe-builders*)))))))
+    (set! *foes* (list-tabulate *foes-num* build-foe))))
+
+(define (foe-dead? f ::foe)
+  (<= f:health 0))
+
+(define (show-foes)
+  (output "Your foes:\n")
+  (for-each (lambda (x)
+	      (let ((f ::foe (list-ref *foes* x)))
+		(output (+ x 1))
+		(output ". ")
+		(if (foe-dead? f)
+		    (output "**dead**\n")
+		    (begin (output "(Health = ")
+			   (output f:health)
+			   (output ") ")
+			   (f:show)))))
+	    (list-tabulate *foes-num* values)))
+
+;; Foes
+(define-simple-class foe ()
+  (health)
+  ((*init*)
+   (set! health (randval 10)))
+  ((show) #!abstract))
+
+(define-simple-class orc (foe)
+  (club-level)
+  ((*init*)
+   (invoke-special foe (this) '*init*)
+   (set! club-level (randval 8)))
+  ((show)
+   (output "A wicked orc with a level ")
+   (output club-level)
+   (output " club\n")))
+
+(define-simple-class hydra (foe)
+  ((show)
+   (output "A malicious hydra with ")
+   (output health)
+   (output " heads\n")))
+
+(define-simple-class slime (foe)
+  (sliminess)
+  ((*init*)
+   (invoke-special foe (this) '*init*)
+   (set! sliminess (randval 5)))
+  ((show)
+   (output "A slime with a sliminess of ")
+   (output sliminess)
+   (output "\n")))
+
+(define-simple-class brigand (foe)
+  ((show)
+   (output "A fierce brigand\n")))
