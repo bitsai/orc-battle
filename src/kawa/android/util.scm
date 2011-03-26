bc21887524e545928523f3ed5c84fa99e421df1e
Benny Tsai <benny.tsai@gmail.com>
2011-03-26 02:02:31 -0600

    create utility module, refactor game code

---
+++ src/kawa/android/util.scm
@@ -0,0 +1,43 @@
+(define (rand-int n)
+  (*:nextInt (java.util.Random) n))
+
+(define (rand-nth-list lst)
+  (list-ref lst (rand-int (length lst))))
+
+(define-syntax dolist
+  (syntax-rules ()
+    ((dolist (counter init) body ...)
+     (for-each (lambda (counter)
+		 body ...)
+	       init))
+    ((dolist (counter ::type init) body ...)
+     (for-each (lambda (counter ::type)
+		 body ...)
+	       init))))
+
+(define-syntax dotimes
+  (syntax-rules ()
+    ((dotimes (counter init) body ...)
+     (do ((max init)
+	  (counter 0 (inc counter)))
+	 ((= counter max))
+       body ...))))
+
+(define (inc n)
+  (+ n 1))
+
+(define (dec n)
+  (- n 1))
+
+(define (nil? x)
+  (eqv? #!null x))
+
+(define (type-of obj)
+  (let ((n (*:getName (*:getClass obj))))
+    (substring n (string-length "kawa.android.") (string-length n))))
+
+(define (str . xs)
+  (let ((o (open-output-string)))
+    (dolist (x xs)
+	    (display x o))
+    (get-output-string o)))
